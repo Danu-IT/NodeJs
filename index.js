9f9c12fe843c43ba16d289f6fce3fa73ee61c3f2
Daniil <d_tareev@mail.ru>
2023-01-06 17:15:19 +0300

    task4Completed

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 import fs from "fs";
-import { Transform } from "stream";
+import inquirer from "inquirer";
+import path from "path";
+import fsp from "fs/promises";
+import colors from "colors";
 
 import * as readline from "readline";
 
-const file = readline.createInterface({
-  input: fs.createReadStream("access_tmp.log.txt"),
-  output: process.stdout,
-  terminal: false,
-});
+const __dirname = "/Портфолио/NodeJs";
 
-file.on("line", (line) => {
-  if (line == "") return false;
-  const id = line.slice(0, line.indexOf("-") - 1);
-  const writeStream = fs.createWriteStream("./%ip-адрес%_requests.log", {
-    flags: "a",
+fsp
+  .readdir(path.join(__dirname))
+  .then(async (inDir) => {
+    const list = [];
+    for (const item of inDir) {
+      const src = await fsp.stat(item);
+      if (src.isFile()) list.push(item);
+    }
+    return list;
+  })
+  .then((list) => {
+    return inquirer.prompt({
+      name: "fileName",
+      type: "list",
+      message: "Choose file",
+      choices: list,
+    });
+  })
+  .then(({ fileName }) => {
+    const find = process.argv[2];
+    const file = readline.createInterface({
+      input: fs.createReadStream(fileName),
+      output: process.stdout,
+      terminal: false,
+    });
+    let count = 0;
+    let lines = [];
+    let lineCount = 0;
+
+    file.on("line", (line) => {
+      lineCount++;
+      if (line.indexOf(find) !== -1) {
+        count++;
+        lines.push(lineCount);
+      }
+    });
+    file.on("pause", () => {
+      console.log(`Total occurrences ${colors.blue(count)} registered`);
+      console.log(`On the lines ${colors.green(lines.join(" "))}`);
+    });
+    file.on("close", () => console.log(`${colors.red("End of calculations")}`));
   });
-  if (id == "89.123.1.41" || id == "34.48.240.111") {
-    writeStream.write(`${line}\n`);
-  }
-});
